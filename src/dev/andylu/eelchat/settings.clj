dd172369d9ba0f8430a608541fc74f75e32680c1
Andy Lu <luandy64@gmail.com>
2023-10-17 19:58:23 -0400

    First commit

---
+++ src/dev/andylu/eelchat/settings.clj
@@ -0,0 +1,3 @@
+(ns dev.andylu.eelchat.settings)
+
+(def app-name "My Application")
---
+++ src/dev/andylu/eelchat/test.clj
@@ -0,0 +1,5 @@
+(ns dev.andylu.eelchat.test
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest example-test
+  (is (= 4 (+ 2 2))))
